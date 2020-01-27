c9015867b6a1b7125039fd44dfe450c841636ca5
sawayu <57563734+sawayu@users.noreply.github.com>
2020-01-28 04:23:14 +0900

    add : グループの作成実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,21 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new (group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループが作成されたよ！"
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 
 end
