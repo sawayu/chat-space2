b6049cfc8ec3e1e15bb19398d8de36e1017270bc
sawayu <57563734+sawayu@users.noreply.github.com>
2020-02-02 16:29:37 +0900

    add :

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,34 @@
 class MessagesController < ApplicationController
+  before_action :set_groups
+  # before_action :set_users
 
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :name).merge(user_id: current_user.id )
+  end
+
+  def set_groups
+    @group = Group.find(params[:group_id])
+  end
+
+  # def set_users
+  #   @user = User.find(params[user_id])
+  # end
 
 end
